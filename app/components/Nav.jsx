33a67a13638d9ea6dafe3bc945b883c55e51e265
Jason Clifford <jason@ukfsn.org>
2017-06-03 16:25:30 +0100

    Initial Commit - just a nav bar

---
+++ app/components/Nav.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function () {
+    return (
+      <div className='top-bar'>
+        <div className='top-bar-left'>
+          <ul className='menu'>
+            <li className='menu-text'>React Timer</li>
+            <li>
+              <IndexLink to='/' activeClassName='active-link'>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to='/countdown' activeClassName='active-link'>Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='top-bar-right'>
+          <ul className='menu'>
+            <li className='menu-text'>By <a href='http://www.jasonclifford.com/'>Jason Clifford</a></li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Nav;
